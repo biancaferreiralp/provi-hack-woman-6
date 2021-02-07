5975d907ad9ba0154820c5966f1e36015a6d613f
Bianca Ferreira <biancaferreiralp@gmail.com>
2021-02-07 19:23:32 -0300

    feat: Implemented Cloudnary and added images to seeds

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'pg_search', '~> 2.3.0'
 
 gem 'dotenv-rails', groups: [:development, :test]
 
+gem 'cloudinary', '~> 1.16.0'
+
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "open-uri"
+
+
 # USERS/COMPANIES
 
 provi = User.create!(company_name: "Provi", website: "provi.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, React Native, Redux, Bootstrap e jQuery.", 
                       email: "exemplo@provi.com", password: "123456")
+                      url = 'https://i.imgur.com/RRlRfqZ.png'
+                      filename = File.basename(URI.parse(url).path)
+                      file = URI.open(url)
+                      provi.photo.attach(io: file, filename: filename)
+                      provi.save!
 
 grupo_boticario = User.create!(company_name: "Grupo Boticário", website: "grupoboticario.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, Angular, Redux, Bootstrap e jQuery.", 
                       email: "exemplo@grupoboticario.com", password: "123456")
+                      url = 'https://i.imgur.com/iq1eQFU.jpg'
+                      filename = File.basename(URI.parse(url).path)
+                      file = URI.open(url)
+                      grupo_boticario.photo.attach(io: file, filename: filename)
+                      grupo_boticario.save!
 
 wavy = User.create!(company_name: "Wavy", website: "wavy.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, Vue.js, Redux, Bootstrap e jQuery.", 
                           email: "exemplo@wavy.com", password: "123456")
+                          url = 'https://i.imgur.com/bJP4Xo2.png'
+                          filename = File.basename(URI.parse(url).path)
+                          file = URI.open(url)
+                          wavy.photo.attach(io: file, filename: filename)
+                          wavy.save!
 
 pagar_me = User.create!(company_name: "Pagar.me", website: "pagar.me", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, Redux, Bootstrap e jQuery.", 
                           email: "exemplo@pagarme.com", password: "123456")
+                          url = 'https://i.imgur.com/9IM0pyN.png'
+                        filename = File.basename(URI.parse(url).path)
+                        file = URI.open(url)
+                        pagar_me.photo.attach(io: file, filename: filename)
+                        pagar_me.save!
 
 credere = User.create!(company_name: "Credere", website: "credere.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, React Native, Bootstrap e jQuery.", 
                           email: "exemplo@credere.com", password: "123456")
+                          url = 'https://i.imgur.com/AnGJ8Rs.png'
+                          filename = File.basename(URI.parse(url).path)
+                          file = URI.open(url)
+                          credere.photo.attach(io: file, filename: filename)
+                          credere.save!
 
 enjoei = User.create!(company_name: "Enjoei", website: "enjoei.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, React Native, Redux, Bootstrap e jQuery.", 
                           email: "exemplo@enjoei.com", password: "123456")
+                          url = 'https://i.imgur.com/H91yux3.jpg'
+                          filename = File.basename(URI.parse(url).path)
+                          file = URI.open(url)
+                          enjoei.photo.attach(io: file, filename: filename)
+                          enjoei.save!
 
 
 # JOBS
