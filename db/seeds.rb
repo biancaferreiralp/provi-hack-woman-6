55e7f14485ffb086265feada7d5cdb8481aae61c
Bianca Ferreira <biancaferreiralp@gmail.com>
2021-02-07 22:29:40 -0300

    Integrated front-end code to Jobs show view and did minor changes to seeds, updated application name and remove images not used - Page code by https://github.com/leticiafs

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ require "open-uri"
 
 # USERS/COMPANIES
 
-provi = User.create!(company_name: "Provi", website: "provi.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, React Native, Redux, Bootstrap e jQuery.", 
+provi = User.create!(company_name: "Provi", website: "provi.com", description: "Todos os dias, trabalhamos para superar desafios, trocar experiências e aprender cada vez mais. Somos mais de 400 pessoas, que colaboram, transformam e caminham na direção do nosso propósito, que é trazer soluções inovadoras para melhorar a vida de cada cliente.", 
                       email: "exemplo@provi.com", password: "123456")
                       url = 'https://i.imgur.com/RRlRfqZ.png'
                       filename = File.basename(URI.parse(url).path)
@@ -19,7 +19,7 @@ provi = User.create!(company_name: "Provi", website: "provi.com", description: "
                       provi.photo.attach(io: file, filename: filename)
                       provi.save!
 
-grupo_boticario = User.create!(company_name: "Grupo Boticário", website: "grupoboticario.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, Angular, Redux, Bootstrap e jQuery.", 
+grupo_boticario = User.create!(company_name: "Grupo Boticário", website: "grupoboticario.com", description: "Todos os dias, trabalhamos para superar desafios, trocar experiências e aprender cada vez mais. Somos mais de 400 pessoas, que colaboram, transformam e caminham na direção do nosso propósito, que é trazer soluções inovadoras para melhorar a vida de cada cliente.", 
                       email: "exemplo@grupoboticario.com", password: "123456")
                       url = 'https://i.imgur.com/iq1eQFU.jpg'
                       filename = File.basename(URI.parse(url).path)
@@ -27,7 +27,7 @@ grupo_boticario = User.create!(company_name: "Grupo Boticário", website: "grupo
                       grupo_boticario.photo.attach(io: file, filename: filename)
                       grupo_boticario.save!
 
-wavy = User.create!(company_name: "Wavy", website: "wavy.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, Vue.js, Redux, Bootstrap e jQuery.", 
+wavy = User.create!(company_name: "Wavy", website: "wavy.com", description: "Todos os dias, trabalhamos para superar desafios, trocar experiências e aprender cada vez mais. Somos mais de 400 pessoas, que colaboram, transformam e caminham na direção do nosso propósito, que é trazer soluções inovadoras para melhorar a vida de cada cliente.", 
                           email: "exemplo@wavy.com", password: "123456")
                           url = 'https://i.imgur.com/bJP4Xo2.png'
                           filename = File.basename(URI.parse(url).path)
@@ -35,7 +35,7 @@ wavy = User.create!(company_name: "Wavy", website: "wavy.com", description: "Ent
                           wavy.photo.attach(io: file, filename: filename)
                           wavy.save!
 
-pagar_me = User.create!(company_name: "Pagar.me", website: "pagar.me", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, Redux, Bootstrap e jQuery.", 
+pagar_me = User.create!(company_name: "Pagar.me", website: "pagar.me", description: "Todos os dias, trabalhamos para superar desafios, trocar experiências e aprender cada vez mais. Somos mais de 400 pessoas, que colaboram, transformam e caminham na direção do nosso propósito, que é trazer soluções inovadoras para melhorar a vida de cada cliente.", 
                           email: "exemplo@pagarme.com", password: "123456")
                           url = 'https://i.imgur.com/9IM0pyN.png'
                         filename = File.basename(URI.parse(url).path)
@@ -43,7 +43,7 @@ pagar_me = User.create!(company_name: "Pagar.me", website: "pagar.me", descripti
                         pagar_me.photo.attach(io: file, filename: filename)
                         pagar_me.save!
 
-credere = User.create!(company_name: "Credere", website: "credere.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, React Native, Bootstrap e jQuery.", 
+credere = User.create!(company_name: "Credere", website: "credere.com", description: "Todos os dias, trabalhamos para superar desafios, trocar experiências e aprender cada vez mais. Somos mais de 400 pessoas, que colaboram, transformam e caminham na direção do nosso propósito, que é trazer soluções inovadoras para melhorar a vida de cada cliente.", 
                           email: "exemplo@credere.com", password: "123456")
                           url = 'https://i.imgur.com/AnGJ8Rs.png'
                           filename = File.basename(URI.parse(url).path)
@@ -51,7 +51,7 @@ credere = User.create!(company_name: "Credere", website: "credere.com", descript
                           credere.photo.attach(io: file, filename: filename)
                           credere.save!
 
-enjoei = User.create!(company_name: "Enjoei", website: "enjoei.com", description: "Entre os requisitos desta vaga estão HTML, CSS/SCSS/SASS, JavaScript, React JS, React Native, Redux, Bootstrap e jQuery.", 
+enjoei = User.create!(company_name: "Enjoei", website: "enjoei.com", description: "Todos os dias, trabalhamos para superar desafios, trocar experiências e aprender cada vez mais. Somos mais de 400 pessoas, que colaboram, transformam e caminham na direção do nosso propósito, que é trazer soluções inovadoras para melhorar a vida de cada cliente.", 
                           email: "exemplo@enjoei.com", password: "123456")
                           url = 'https://i.imgur.com/H91yux3.jpg'
                           filename = File.basename(URI.parse(url).path)
